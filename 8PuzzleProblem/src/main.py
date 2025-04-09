b484ef04563402b92bba44104cc381f1457973cd
Thiago Rodrigues <thiagoneye@outlook.com>
2025-04-09 00:12:24 -0300

    Update code
    Apagado linhas de teste do código.

--- 8PuzzleProblem/src/main.py
+++ 8PuzzleProblem/src/main.py
@@ -8,11 +8,3 @@ from heuristic import greedy_algorithm, prints_the_movement_history
 if __name__ == "__main__":
     puzzle = Puzzle()
     
-    for value in range(1, 9):
-        print(value)
-    
-    #moves, list_of_states = greedy_algorithm(puzzle)
-    
-    #print("\n\n\nO algoritmo finalizou!\n")
-    
-    #prints_the_movement_history(moves, list_of_states)
